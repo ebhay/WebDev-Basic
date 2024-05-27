818add805d3020bcd562f0c1b3b727c60bd25055
Abhay Gupta <111756624+thisabhay@users.noreply.github.com>
2024-05-27 18:04:00 +0530

    Add initial HTML structure for the Color Picker page in day23/index.html

--- day23/app.js
+++ day23/app.js
@@ -1,7 +1,21 @@
 let btn= document.querySelector("button");  
+btn.addEventListener("click",function() {
+    let h1=document.querySelector("h1");
+    let str=colr();
+    h1.innerText=str;
+});
+
+btn.addEventListener("click",function() {
+    let div=document.querySelector("div");
+    let str=colr();
+    div.style.backgroundColor=str;
+});
 
 function colr(){
-    console.log("Generate a random color")
+    let r=Math.floor(Math.random() * 255);
+    let g=Math.floor(Math.random() * 255);
+    let b=Math.floor(Math.random() * 255);
+    let col=`rgb(${r},${g},${b})`;
+    return col;
 }
 
-btn.addEventListener("click",colr);
