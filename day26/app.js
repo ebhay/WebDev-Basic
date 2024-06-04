a065259a69155094140e7ead2e8728fa24d9260c
Abhay Gupta <111756624+thisabhay@users.noreply.github.com>
2024-06-04 20:13:43 +0530

    Api Fetch Process

--- day26/app.js
+++ day26/app.js
@@ -1,4 +1,48 @@
+/*
 async function greet(){
     return "hello world";
 }
 greet.then().catch();
+
+*/
+/*
+General Purpose
+
+let url="https://catfact.ninja/fact";
+fetch(url)
+.then((res)=>{
+    console.log(res);
+    return res.json();
+})
+.then((result)=>{
+    console.log(result);
+    console.log(result.fact);
+    return fetch(url)
+})
+
+.then((res)=>{
+    console.log(res);
+    return res.json();
+})
+.then((result)=>{
+    console.log(result);
+    console.log(result.fact);})
+
+.catch((error)=>{
+    console.log(error);
+})*/
+
+//Using await and async functions
+
+let url="https://catfact.ninja/fact";
+async function getFacts(){
+    try{
+        let res=await fetch(url);
+        let result=await res.json();
+        console.log(result.fact);
+    }
+    catch{
+        console.log("Error Occured:"+error);
+    }
+}
+getFacts();
