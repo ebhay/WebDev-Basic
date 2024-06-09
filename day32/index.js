b6ba6e19f2231c0746c40ec78c00c2f28c0e859b
Abhay Gupta <111756624+thisabhay@users.noreply.github.com>
2024-06-09 20:37:17 +0530

    Update index.js

--- day32/index.js
+++ day32/index.js
@@ -5,7 +5,7 @@ const port = 8080;
 const path = require('path');
 
 app.set("view engine", "ejs");
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname,"/public")));
 app.set("views",path.join(__dirname,"./views"));
 //Now We Will Access data from data base
 app.get("/ig/:username", (req, res) => {
